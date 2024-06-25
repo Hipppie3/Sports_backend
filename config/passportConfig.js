916d21f93582f4c66dd522b056bf05a5c5be8884
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2024-06-25 16:00:43 -0700

    Update passport to use database for authentication

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -1,38 +1,42 @@
 import passport from 'passport';
 import { Strategy as LocalStrategy } from 'passport-local';
 import bcrypt from 'bcrypt';
+import pool from './db/pool.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 // Serialize user into the session
 passport.serializeUser((user, done) => {
-  done(null, user.username);
+  done(null, user.id);
 });
 
 // Deserialize user from the session
-passport.deserializeUser((username, done) => {
-  const user = { username }; // Since we have only one user, we can just return the username
-  done(null, user);
+passport.deserializeUser((id, done) => {
+  pool.query('SELECT id, username FROM users WHERE id = $1', [id], (err, result) => {
+    if (err) return done(err);
+    done(null, result.rows[0]);
+  });
 });
 
 // Configure local strategy for passport
 passport.use(new LocalStrategy((username, password, done) => {
-  const hardcodedUsername = process.env.ADMIN_USERNAME;
-  const storedHashedPassword = process.env.ADMIN_PASSWORD;
-
-  if (username !== hardcodedUsername) {
-    return done(null, false, { message: 'Incorrect username.' });
-  }
-
-  bcrypt.compare(password, storedHashedPassword, (err, res) => {
+  pool.query('SELECT id, username, password FROM users WHERE username = $1', [username], (err, result) => {
     if (err) return done(err);
-    if (res) {
-      const user = { username }; // Use the hardcoded username as the user object
-      return done(null, user);
-    } else {
-      return done(null, false, { message: 'Incorrect password.' });
+    if (result.rows.length === 0) {
+      return done(null, false, { message: 'Incorrect username.' });
     }
+
+    const user = result.rows[0];
+
+    bcrypt.compare(password, user.password, (err, res) => {
+      if (err) return done(err);
+      if (res) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+    });
   });
 }));
 
